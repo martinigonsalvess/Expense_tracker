dad6ca9150ecd7f9f074a1537a677761ac1218bd
martinigonsalves <32884454+martinigonsalves@users.noreply.github.com>
2021-04-22 09:59:54 +0200

    delete button from expenseItem

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -17,7 +17,6 @@ const ExpenseItem = (props) => {
         <h2>{title}</h2>
         <div className="expense-item__price">€ {props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 };
